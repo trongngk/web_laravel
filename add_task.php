19abfe33a365dfa269c56a51dadd8ada8eafe896
trongngk <53163510+trongngk@users.noreply.github.com>
2022-08-22 21:36:12 +0700

    Delete add_task.php

--- add_task.php
+++
@@ -1,9 +0,0 @@
-<?php
-    ob_start();
-    include "connect.php";
-    $task = $_POST['task'];
-    $sql = "INSERT INTO content (task) VALUES ('$task')";
-    $query = mysqli_query($conn, $sql);
-    header("location:inside_page.php");
-    ob_end_flush();
-?>
